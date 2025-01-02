68d88a6a3017cc28c7be45518e503c66faa4936f
mobadran <mohamedbadran1492008@gmail.com>
2025-01-02 15:53:08 +0200

    Finished styling and functionality

--- scripts/script.js
+++ scripts/script.js
@@ -107,21 +107,25 @@ data.forEach(element => {
 	items.innerHTML += `
             		<div>
 						<div class="relative mb-8 has-[.add-to-cart.hidden]:border-red border-2 rounded-xl">
-							<img src="${element.image.desktop}" alt="Baklava" class="rounded-xl w-72 lg:block hidden">
-							<img src="${element.image.tablet}" alt="Baklava" class="rounded-xl w-72 sm:block lg:hidden hidden">
-							<img src="${element.image.mobile}" alt="Baklava" class="rounded-xl w-72 max-sm:block hidden">
-							<div class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 min-w-32">
-								<button class="flex gap-3 bg-white py-3 px-8 pr-14 rounded-full border-rose-300 border add-to-cart" onclick="addToCart(${data.indexOf(element)})">
-									<img src="./assets/images/icon-add-to-cart.svg" alt="">
+							<img src="${element.image.desktop}" class="rounded-xl w-72 lg:block hidden">
+							<img src="${element.image.tablet}" class="rounded-xl w-72 sm:block lg:hidden hidden">
+							<img src="${element.image.mobile}" class="rounded-xl w-72 max-sm:block hidden">
+							<div class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 min-w-40">
+								<button class="flex gap-3 bg-white py-3 px-8 pr-14 rounded-full border-rose-300 border add-to-cart hover:text-red hover:border-red" onclick="addToCart(${data.indexOf(element)})">
+									<img src="./assets/images/icon-add-to-cart.svg">
 									<p class="font-semibold text-nowrap">Add To Cart</p>
 								</button>
 								<div class="flex justify-between bg-red py-3 px-4 w-full rounded-full border-rose-300 border added-to-cart hidden">
 									<button onclick="decrement(${data.indexOf(element)})">
-										<img src="./assets/images/icon-decrement-quantity.svg" alt="minus" class="rounded-full border-white border p-1 w-5 h-5">
+										<svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" viewBox="0 0 10 2" class="rounded-full border-white border p-1 w-5 h-5 hover:bg-white hover:text-red text-white">
+                                        	<path fill="currentColor" d="M0 .375h10v1.25H0V.375Z" />
+                                    	</svg>
 									</button>
 									<p class="cart-number text-white">1</p>
 									<button onclick="increment(${data.indexOf(element)})">
-										<img src="./assets/images/icon-increment-quantity.svg" alt="plus" class="rounded-full border-white border p-1 w-5 h-5">
+										<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10" class="rounded-full border-white border p-1 w-5 h-5 hover:bg-white hover:text-red text-white">
+                                        	<path fill="currentColor" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z" />
+                                    	</svg>
 									</button>
 								</div>
 							</div>
@@ -163,19 +167,21 @@ function display(id) {
 }
 
 function displayNum(id) {
-	document.querySelector(`.added-to-cart:has(button[onclick="decrement(${id})"]) .cart-number`).innerText = cart.filter(obj => obj.id === id)[0].quantity;
-	cart.filter(obj => obj.id === id)[0].quantity === 0 ? cartZero(id) : cartOne(id);
-	// Remove object from cart
+	if (document.querySelector(`.added-to-cart:has(button[onclick="decrement(${id})"]) .cart-number`)) {
+		document.querySelector(`.added-to-cart:has(button[onclick="decrement(${id})"]) .cart-number`).innerText = cart.filter(obj => obj.id === id)[0] ? cart.filter(obj => obj.id === id)[0].quantity : 1;
+	}
+	// Remove object from cart if quantity = 0
 	cart.filter(obj => obj.quantity === 0).forEach(obj => {
 		cart.splice(cart.indexOf(obj), 1);
-	})
-	console.log(cart);
-	// displayCart();
+	});
+	!cart.filter(obj => obj.id === id)[0] ? quantityZero(id) : quantityOne(id);
+	!cart[0] ? cartZero(id) : cartOne(id);
 }
 
 function displayCart() {
 	var element;
 	document.getElementById('cart').innerHTML = '';
+	document.getElementById('orderConfirmed').innerHTML = '';
 	cart.forEach(obj => {
 		for (let i in data) {
 			if (i == obj.id) {
@@ -183,38 +189,88 @@ function displayCart() {
 			}
 		}
 		document.getElementById('cart').innerHTML += `
-		<div class="flex justify-between w-full border-b py-2">
+					<div class="flex justify-between w-full border-b py-2">
                         <div class="grid grid-rows-2 grid-cols-3 gap-2">
-                            <h4 class="col-start-1 col-end-4 font-semibold">${element.name}</h4>
-                            <span class="text-red font-semibold">${obj.quantity}x</span>
+							<h4 class="col-start-1 col-end-4 font-semibold">${element.name}</h4>
+							<span class="text-red font-semibold">${obj.quantity}x</span>
                             <span class="text-rose-400">@ $${element.price.toFixed(2)}</span>
                             <span class="text-rose-500 font-semibold">$${(element.price * obj.quantity).toFixed(2)}</span>
                         </div>
-                        <button onclick="removeItem(0)">
-                            <img src="./assets/images/icon-remove-item.svg" alt="close" class="border w-6 h-6 p-1 rounded-full">
+                        <button onclick="removeItem(${obj.id})">
+							<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10" class="text-rose-300 border border-rose-300 w-6 h-6 p-1 rounded-full hover:border-rose-900 hover:text-rose-900">
+                        		<path fill="currentColor" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z" />
+                            </svg>
                         </button>
                     </div>
-		`
-	})
+		`;
+
+		document.getElementById('orderConfirmed').innerHTML += `
+			<div class="flex gap-2 border-b p-2 items-center mb-5">
+                <img src="${element.image.thumbnail}" class="w-14 h-14">
+                <div class="flex flex-col justify-between w-full">
+                    <h3 class="text-rose-900">${element.name}</h3>
+                    <div>
+                        <span class="text-red font-semibold">${obj.quantity}x</span>
+                        <span class="text-rose-400">@ $${element.price.toFixed(2)}</span>
+                    </div>
+                </div>
+                <span class="text-rose-900 font-semibold">$${(element.price * obj.quantity).toFixed(2)}</span>
+            </div>
+		`;
+	});
+	let total = 0;
+	let quantity = 0;
+	cart.forEach(obj => {
+		total += obj.quantity * data[obj.id].price;
+		quantity += obj.quantity;
+	});
+	document.getElementById('total').innerText = total;
+	document.getElementById('totalConfirmed').innerText = total;
+	// Display Total Quantity
+	document.getElementById('quantity').innerText = quantity;
 }
 
 function removeItem(id) {
 	cart.splice(cart.indexOf(cart.filter(obj => obj.id == id)[0]), 1);
-	display();
+	display(id);
 }
 
-function cartOne(id) {
-	document.querySelector(`.add-to-cart[onclick="addToCart(${id})"]`).classList.add('hidden')
-	document.querySelector(`.added-to-cart:has(button[onclick="decrement(${id})"])`).classList.remove('hidden')
-
-	document.getElementById('cartContainer').classList.remove('hidden')
-	document.getElementById('emptyCart').classList.add('hidden')
+function quantityOne(id) {
+	document.querySelector(`.add-to-cart[onclick="addToCart(${id})"]`).classList.add('hidden');
+	document.querySelector(`.added-to-cart:has(button[onclick="decrement(${id})"])`).classList.remove('hidden');
 }
 
-function cartZero(id) {
-	document.querySelector(`.add-to-cart[onclick="addToCart(${id})"]`).classList.remove('hidden')
-	document.querySelector(`.added-to-cart:has(button[onclick="decrement(${id})"])`).classList.add('hidden')
+function cartOne() {
+	document.getElementById('cartContainer').classList.remove('hidden');
+	document.getElementById('emptyCart').classList.add('hidden');
+}
 
-	document.getElementById('emptyCart').classList.remove('hidden')
-	document.getElementById('cartContainer').classList.add('hidden')
+function quantityZero(id) {
+	document.querySelector(`.add-to-cart[onclick="addToCart(${id})"]`).classList.remove('hidden');
+	document.querySelector(`.added-to-cart:has(button[onclick="decrement(${id})"])`).classList.add('hidden');
+}
+
+function cartZero() {
+	document.getElementById('emptyCart').classList.remove('hidden');
+	document.getElementById('cartContainer').classList.add('hidden');
+}
+
+function confirmOrder() {
+	document.getElementById('confirmation').classList.remove('hidden');
+	document.body.classList.add('overflow-hidden')
+}
+
+function newOrder() {
+	document.getElementById('confirmation').classList.add('hidden');
+	document.body.classList.remove('overflow-hidden')
+	clearCart();
+	displayCart();
+}
+
+function clearCart() {
+	cartZero()
+	cart.forEach(obj => {
+		quantityZero(obj.id);
+	});
+	cart = [];
 }
